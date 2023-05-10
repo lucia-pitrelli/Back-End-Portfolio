deb26e6e1da9222987eff5208aa48d40836b938a
Lucia Pitrelli <pitrelliblucia@gmail.com>
2023-05-10 15:46:38 -0300

    add model and controller of hard skills

---
+++ src/main/java/com/portfolio/Ap/controllers/HardSkillController.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.Ap.controllers;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/hardskills")
+public class HardSkillController {
+         @GetMapping
+    public String getHardSkills(){
+        return "Petition Get a list of Hard Skills";
+    }    
+    
+    @GetMapping("/{id}")
+    public String getHardSkill(@PathVariable Long id){
+        return "Petition Get a Hard Skill number:" + id;
+    }
+    
+    @PostMapping
+    public String postHardSkill(@RequestBody String body){
+        return "Petition Post:" + body;
+    }  
+    
+    @PutMapping("/{id}")
+        public String changeHardSkill(@PathVariable Long id){
+        return "Petition Put a Hard Skill number:" + id;
+    }
+    
+    @DeleteMapping("/{id}")
+    public String deleteHardSkill(@PathVariable Long id){
+        return "Do you want to delete a Hard Skills number:" + id;
+    }
+}
---
+++ src/main/java/com/portfolio/Ap/models/HardSkill.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.Ap.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "hardskills")
+public class HardSkill {
+     
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @Column(length=50, nullable = false)
+    private String name;
+    
+    @Column( nullable = false)
+    private int percentage;
+    
+    /*constructor*/
+
+    public HardSkill(Long id, String name, int percentage) {
+        this.id = id;
+        this.name = name;
+        this.percentage = percentage;
+    }
+    
+    
+    /*getter and setter*/
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(int percentage) {
+        this.percentage = percentage;
+    }
+    
+}
