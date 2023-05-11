0c5acf6cd36d344efce24b218bd3ff36513ab38c
Lucia Pitrelli <pitrelliblucia@gmail.com>
2023-05-10 21:42:16 -0300

    add controller and model of softSkill

---
+++ src/main/java/com/portfolio/Ap/controllers/SoftSkillController.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.Ap.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/softskills")
+public class SoftSkillController {
+    
+    /*tengo una sola skill*/
+       public String getSoftSkills(){
+        return "Petition Get a list of Soft Skills";
+    }    
+    
+    @GetMapping("/{id}")
+    public String getSoftSkill(@PathVariable Long id){
+        return "Petition Get a Soft Skill number:" + id;
+    }
+     
+    
+    @PutMapping("/{id}")
+        public String changeSoftSkill(@PathVariable Long id){
+        return "Petition Put a Soft Skills number:" + id;
+    }
+    
+}
---
+++ src/main/java/com/portfolio/Ap/models/SoftSkill.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.Ap.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "softskills")
+public class SoftSkill {
+   @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @Column(length=200, nullable = false)
+    private String text; 
+
+    
+    /*constructor*/
+
+    public SoftSkill(Long id, String text) {
+        this.id = id;
+        this.text = text;
+    }
+    
+    
+    
+    /*getter and setter*/
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+    
+    
+}
